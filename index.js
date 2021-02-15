4a068a6750068dffffb19133223bfbde5444ca19
CaroKris <ckhilpisch@gmail.com>
2021-02-15 16:02:31 -0600

    adding README file to the index.js

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const answers = [];
+const generateREADME = (answers) =>
+`#${answers.appTitle}:
+##Table of Contents:
+<ol>
+<li><a href="#description">Description</a></li>
+<li><a href="#installation">Installation</a></li>
+<li><a href="#techologies">Technologies Used</a></li>
+<li><a href="#usage">Usage</a></li>
+<li><a href="#contributing">Contributing</a></li>
+<li><a href="#testing">Testing</a></li>
+<li><a href="#questions">Questions</a></li>
+<li><a href="#license">License</a></li>
+</ol>
+
+## Description:
+${answers.descript}
+
+##Installation:
+${answers.install}
+
+## Technologies:
+<ul>
+    <li>${answers.tech[0]}
+    <li>${answers.tech[1]}
+    <li>${answers.tech[2]}
+    <li>${answers.tech[3]}
+    <li>${answers.tech[4]}
+</ul>
+
+## Usage: 
+${answers.usage}
+
+## Contributing:
+${ansers.cont}
+
+##Testing:
+${answers.test} 
+
+## Questions:
+Have any questions about the app?  Check out my Github Site:
+
+www.github.com/${answers.github}
+
+Feel free to drop me a line:
+<a class="link" href="mailto:${answers.email}">${answers.email}</a>
+
+## License:
+Distributed under the ${answers.license} license.`
+
+
 
 inquirer
     .prompt ([
@@ -41,7 +90,7 @@ inquirer
     },
     {
         type: 'checkbox',
-        message: 'What is the license for your application',
+        message: 'What is the license for your application?',
         name: 'license',
         choices: ['Apache', 'GNU', 'MIT', 'BSD', 'Unlicense']
     },
@@ -63,7 +112,6 @@ inquirer
         message: 'What is your email address?'
 
     },
-]).then((data)=> {
-    this.data.answers.push
+]).then((answers)=> {
     console.log(answers);
 })

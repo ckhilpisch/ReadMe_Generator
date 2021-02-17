d62d47e926c9f70124270ebd9d009c41363f2df2
CaroKris <ckhilpisch@gmail.com>
2021-02-17 13:48:35 -0600

    changing the module export and the tags inside the license rendering

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,63 @@
 function generateMarkdown(answers) {
+
+    function renderLicenseBadge(license) {
+        let badge;
+            switch(license) {
+                case 'Apache':
+                    badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]';
+                    break;
+                case 'GNU':
+                    badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]';
+                    break;
+                case 'MIT':
+                    badge = 'https://opensource.org/licenses/MIT';
+                    break;
+                case 'BSD':
+                    badge = '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]';
+                    break;
+                case 'Unlicense':
+                    badge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]';
+                    break;
+                default:
+                    badge = '';
+                    break;   
+            }
+            return badge;
+                    
+    }
+    
+    
+    // TODO: Create a function that returns the license link
+    // If there is no license, return an empty string
+    function renderLicenseLink(license) {
+        let link;
+        switch(license) {
+                    case 'Apache':
+                        link = 'https://opensource.org/licenses/Apache-2.0';
+                        break;
+                    case 'GNU':
+                        link = 'https://www.gnu.org/licenses/gpl-3.0';
+                        break;
+                    case 'MIT':
+                        link = 'https://opensource.org/licenses/MIT';
+                        break;
+                    case 'BSD':
+                        link = 'https://opensource.org/licenses/BSD-3-Clause';
+                        break;
+                    case 'Unlicense':
+                        link = 'http://unlicense.org/';
+                        break;
+                    default:
+                        link = '';
+                        break;   
+                }
+                return link;
+            
+    }
+
+
     `#  ${answers.appTitle}:
-    ##  
+    ##  ${renderLicenseBadge()}
     
     ## Table of Contents:
     <ol>
@@ -39,67 +96,75 @@ function generateMarkdown(answers) {
     ## License:
     Distributed under the ${answers.license} license.
     Full license information:
-    <p license></p>
+    Licensed under the ${answers.license} License;
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at:
+    ${renderLicenseLink()}
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
     `
 }
 
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-    let data;
-        switch(license) {
-            case 'Apache':
-                data = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]';
-                break;
-            case 'GNU':
-                data = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]';
-                break;
-            case 'MIT':
-                data = 'https://opensource.org/licenses/MIT';
-                break;
-            case 'BSD':
-                data = '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]';
-                break;
-            case 'Unlicense':
-                data = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]';
-                break;
-            default:
-                data = '';
-                break;   
-        }
-        return data;
-        console.log(data);        
-}
+// function renderLicenseBadge(license) {
+//     let data;
+//         switch(license) {
+//             case 'Apache':
+//                 badge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]';
+//                 break;
+//             case 'GNU':
+//                 badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]';
+//                 break;
+//             case 'MIT':
+//                 badge = 'https://opensource.org/licenses/MIT';
+//                 break;
+//             case 'BSD':
+//                 badge = '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]';
+//                 break;
+//             case 'Unlicense':
+//                 badge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]';
+//                 break;
+//             default:
+//                 badge = '';
+//                 break;   
+//         }
+//         return badge;
+                
+// }
 
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-    let data;
-    switch(license) {
-                case 'Apache':
-                    data = 'https://opensource.org/licenses/Apache-2.0';
-                    break;
-                case 'GNU':
-                    data = 'https://www.gnu.org/licenses/gpl-3.0';
-                    break;
-                case 'MIT':
-                    data = 'https://opensource.org/licenses/MIT';
-                    break;
-                case 'BSD':
-                    data = 'https://opensource.org/licenses/BSD-3-Clause';
-                    break;
-                case 'Unlicense':
-                    data = 'http://unlicense.org/';
-                    break;
-                default:
-                    data = '';
-                    break;   
-            }
-            return data;
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {
+//     let data;
+//     switch(license) {
+//                 case 'Apache':
+//                     link = 'https://opensource.org/licenses/Apache-2.0';
+//                     break;
+//                 case 'GNU':
+//                     link = 'https://www.gnu.org/licenses/gpl-3.0';
+//                     break;
+//                 case 'MIT':
+//                     link = 'https://opensource.org/licenses/MIT';
+//                     break;
+//                 case 'BSD':
+//                     link = 'https://opensource.org/licenses/BSD-3-Clause';
+//                     break;
+//                 case 'Unlicense':
+//                     link = 'http://unlicense.org/';
+//                     break;
+//                 default:
+//                     link = '';
+//                     break;   
+//             }
+//             return link;
         
-}
+// }
 
 
 // TODO: Create a function that returns the license section of README
@@ -127,9 +192,4 @@ function renderLicenseLink(license) {
 
 
 
-module.exports = {
-    renderLicenseLink,
-    renderLicenseBadge,
-    generateMarkdown,
-
-}
+module.exports = generateMarkdown;

6bcf2a3cf2053ab53040203ba1a9dd10aa191c3b
CaroKris <ckhilpisch@gmail.com>
2021-02-17 20:22:26 -0600

    adding to the readme

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-#  The Thing:
-##  
+# README.md Generator
+## 
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
 
 ## Table of Contents:
 <ol>
@@ -7,33 +8,49 @@
 <li><a href="#installation">Installation</a></li>
 <li><a href="#usage">Usage</a></li>
 <li><a href="#contributing">Contributing</a></li>
-<li><a href="#testing">Testing</a></li>
 <li><a href="#questions">Questions</a></li>
 <li><a href="#license">License</a></li>
 </ol>
 
-## Description:
-Things and things
+## Description :
+When using GitHub, it is important to have a professional README available to users.  This information will contain information about the app - how it is used, how it is installed, how to test it, how to contribute, and what license was used.   This README generator will take the stress out of having to make a README for the users.   Simply by entering information about the app via the command line, they can have a wonderful README. 
+Here are the accepatance criteria given:
+![Acceptance Criteria](./assets/require.png)
 
-## Installation:
-put it on a website
 
-## Usage: 
-User can make things happen
+## Installation :
+The application can be run in the termianl inside of your chosen coding application. It uses Javascript and Node as the languages to access it. 
+The application is invoked by using the following command:
+```bash
+node index.js
+```
 
-## Contributing:
-Contribution
+## Usage : 
+The user will open the project inside the chose coding platform.  The user will then run node index.js inside the command line.   After running that code, the user is prompted with questions about the project done.   After answering these questions, the user then will have a professional README created that they can use.
 
-## Testing:
-testing 
 
-## Questions:
-Have any questions about the app?  Check out my Github Site:
+## Contributing :
+Pull requests are always welcome.  When contributing to this repository, please first discuss the change you wish to make via email or issue.  
+After approval, please follow the "fork-and-pull" Git workflow.
+<ol>
+<li>Fork the repo on GitHub</li>
+<li>Clone the project to your own machine</li>
+<li>Commit changes to your own branch</li>
+<li>Push your work back up to your fork</li>
+<li>Submit a Pull request so that we can review your changes</li>
+</ol>
 
-www.github.com/ckhilpisch
+## Questions :
 
-Feel free to drop me a line:
-<a class="link" href="mailto:ckhilpisch@gmail.com">ckhilpisch@gmail.com</a>
+If you have any questions, feel free to reach out to me.   My email is ckhilpisch@gmail.com.
 
-## License:
-Distributed under the MIT license.
+## License :
+
+MIT License
+Informataion avaiable here: 
+https://opensource.org/licenses/MIT
+
+
+
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,60 +1,61 @@
 function generateMarkdown(answers) {
-
-    let license = (answers.license);
-    console.log (license);
-    function renderLicenseBadge() {
-        // let badge;
-            switch(license) {
-                case 'Apache':
-                    badge = '![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
-                    break;
-                case 'GNU':
-                    badge = '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
-                    break;
-                case 'MIT':
-                    badge = 'https://opensource.org/licenses/MIT';
-                    break;
-                case 'BSD':
-                    badge = '![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)';
-                    break;
-                case 'Unlicense':
-                    badge = '![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)';
-                    break;
-                default:
-                    badge = '';
-                    break;   
-            }
-            console.log(badge);
-            return badge;                
+  let license = answers.license;
+  console.log(license);
+  function renderLicenseBadge() {
+    // let badge;
+    switch (license) {
+      case "Apache":
+        badge =
+          "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+        break;
+      case "GNU":
+        badge =
+          "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
+        break;
+      case "MIT":
+        badge = "https://opensource.org/licenses/MIT";
+        break;
+      case "BSD":
+        badge =
+          "![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)";
+        break;
+      case "Unlicense":
+        badge =
+          "![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)";
+        break;
+      default:
+        badge = "";
+        break;
     }
+    return badge;
+  }
 
-    function renderLicenseLink() {
-        // let link;
-        switch(license) {
-            case 'Apache':
-                link = 'https://opensource.org/licenses/Apache-2.0';
-                break;
-            case 'GNU':
-                link = 'https://www.gnu.org/licenses/gpl-3.0';
-                break;
-            case 'MIT':
-                link = 'https://opensource.org/licenses/MIT';
-                break;
-            case 'BSD':
-                link = 'https://opensource.org/licenses/BSD-3-Clause';
-                break;
-            case 'Unlicense':
-                link = 'http://unlicense.org/';
-                break;
-            default:
-                link = '';
-                break;   
-         }
-                return link;
-            
+  function renderLicenseLink() {
+    // let link;
+    switch (license) {
+      case "Apache":
+        link = "https://opensource.org/licenses/Apache-2.0";
+        break;
+      case "GNU":
+        link = "https://www.gnu.org/licenses/gpl-3.0";
+        break;
+      case "MIT":
+        link = "https://opensource.org/licenses/MIT";
+        break;
+      case "BSD":
+        link = "https://opensource.org/licenses/BSD-3-Clause";
+        break;
+      case "Unlicense":
+        link = "http://unlicense.org/";
+        break;
+      default:
+        link = "";
+        break;
     }
+    return link;
+  }
 
-return `#  ${answers.appTitle}:
+  return `#  ${answers.appTitle}:
 ##  ${renderLicenseBadge()}
     
 ## Table of Contents:
@@ -92,18 +93,13 @@ Feel free to drop me a line:
 <a class="link" href="mailto:${answers.email}">${answers.email}</a>
 
 ## License:
-Distributed under the ${answers.license} license.
-Full license information:
-Licensed under the ${answers.license} License;
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at:
+Distributed under the ${answers.license} license.  Full license information:
+Licensed under the ${answers.license} License; you may not use this file except in compliance with the License. You may obtain a copy of the License at:
+
 ${renderLicenseLink()}
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-`
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
+`;
 }
 
 module.exports = generateMarkdown;
